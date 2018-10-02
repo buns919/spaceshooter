853828a450e8dc2d7e93093e0fb0cbd30197c198
buns919 <sbehun07+1@gmail.com>
2018-10-01 20:52:09 -0400

    move enemy towards waypoints and destroy when it reaches the last one

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
 
-    [SerializeField] List<Transform> enemyWayPoints;
+    [SerializeField] List<Transform> enemyWaypoints;
+    [SerializeField] float enemyMoveSpeed = 5f;
+
+    int waypointIndex = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        // start at position 0
+        transform.position = enemyWaypoints[waypointIndex].transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Move();
+    }
+
+    private void Move() {
+        if (waypointIndex <= enemyWaypoints.Count - 1) {
+            var targetPosition = enemyWaypoints[waypointIndex].transform.position;
+            var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition) {
+                waypointIndex++;
+            }
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
 }
